8844124989b81c7ec2826f0fa373d5eb4cde71bf
JakobRF <jste@itu.dk>
2016-10-20 11:24:12 +0200

    Add files via upload
    Nu virker det for én fil! 
    
    Mangler at kunne læse nye filer, men ved ikke, om det er nødvendigt at gøre maskinelt frem for blot at ændre koden fra kørsel til kørsel, når der nu ikke er flere filer end dét.
    
    Der bliver ikke frasorteret nogen data, og jeg mangler at skrive det ud i et nyt dataset.

--- dataCleaner.java
+++ dataCleaner.java
@@ -1,4 +1,8 @@
-import org.json.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.util.Iterator;
@@ -6,41 +10,79 @@ import java.util.Iterator;
 
 public class dataCleaner {
 
-	public static void main(String[] args) throws JSONException {
-
-        	String input = "{\"uuid\":\"e5c91e0d-6515-5b25-adc0-55743c5d371d\",\"Readings\":[[1475479748000,1],[1475479748000,1],[1475479808000,1],[1475479808000,1],[1475479868000,0],[1475480335000,1],[1475480395000,1],[1475480455000,1],[1475480515000,2],[1475480575000,2],[1475480635000,2],[1475480695000,2],[1475480755000,2],[1475480816000,1],[1475480875000,1],[1475480935000,1],[1475480995000,1],[1475481055000,3],[1475481115000,2],[1475481175000,3],[1475481235000,4],[1475481295000,4],[1475481355000,4],[1475481415000,7],[1475481475000,14],[1475482953000,37],[1475482954000,38],[1475483421000,38],[1475483519000,40],[1475484505000,42],[1475484579000,44],[1475484639000,45],[1475484699000,45],[1475484759000,45],[1475484819000,44],[1475484879000,44],[1475484939000,33],[1475484999000,31],[1475485059000,24],[1475485119000,23],[1475485179000,23],[1475485239000,23],[1475485299000,22],[1475485359000,23],[1475485359000,23],[1475488022000,7],[1475488023000,7],[1475488259000,11],[1475488260000,11],[1475488775000,36],[1475489001000,55],[1475489116000,64],[1475489177000,66],[1475489237000,66],[1475489297000,67],[1475489357000,65],[1475489417000,67],[1475489477000,66],[1475489537000,63],[1475489597000,63],[1475493357000,61]]}";
-        			//new JSONObject("/Users/BA9931/Desktop/dataTest.txt");
-        	
-        	JSONObject timeSeries = new JSONObject(input);
-        	
-        	String uuid = timeSeries.getString("uuid");
-        	
-        	System.out.println("uuid = " + uuid);
-   
-    		JSONArray readings = timeSeries.getJSONArray("Readings");
-    		
-    		System.out.println("Readings = " + readings);
-    		
-    		JSONArray values;
-    		
-    		long[] timestamp = new long[readings.length()];
-    		int[] logons = new int[readings.length()];
-    		
-    		int totalLogons = 0;
-    		
-    		for (int i = 0; i < readings.length(); i++) {
-    			values = readings.getJSONArray(i);
-    			
-    			timestamp[i] = values.getLong(0);
-    			logons[i] = values.getInt(1);
-    			
-    			System.out.println("Values " + values);
-    			System.out.println("Timestamp " + timestamp[i] + " logons " + logons[i]);
-    			
-    			totalLogons += logons[i]; 
-    		}
-    		
-    		System.out.println("Total logons today = " + totalLogons);
+	public static void main(String[] args) {
+		
+		JSONParser parser = new JSONParser();
+		JSONArray device = new JSONArray();
+		
+		try{
+			try {
+				device = (JSONArray) parser.parse(new FileReader("C:/Users/BA9931/Desktop/2016-10-04.json"));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} finally {
+			
+		}
+		
+		Iterator<String> iterator1 = device.iterator();
+		
+		int m = 0;
+		while (iterator1.hasNext() && m < device.size()) {
+		
+			JSONObject readingsObj = (JSONObject) device.get(m); 
+			
+			String uuid = (String) readingsObj.get("uuid");
+			JSONArray readings = (JSONArray) readingsObj.get("Readings");
+			
+			// JSONObject uuid = (JSONObject) device.get(1);
+			
+			System.out.println("Readings = " + readings);
+			System.out.println("uuid = " + uuid);
+			
+			JSONArray values;
+	    	
+	    	long[] timestamp = new long[readings.size()+1];
+	    	long[] logons = new long[readings.size()+1];
+	    	
+	    	int totalLogons = 0;
+	    	
+	    	Iterator<String> iterator2 = readings.iterator();
+	    	
+	    	int i = 0;
+	    	while(iterator2.hasNext() && i < readings.size()) {
+	    		values = (JSONArray) readings.get(i);
+	    		
+	    		timestamp[i] = (long) values.get(0);
+	    		logons[i] = (long) values.get(1);
+	    		
+	    		//System.out.println("Values " + values);
+	    		//System.out.println("Timestamp " + timestamp[i] + " logons " + logons[i]);
+	    		
+	    		if (i > 0) {							// 30 sec
+		    		if (timestamp[i] - timestamp[i-1] < 30000) {
+		    			System.out.println("Input " + i + " should be erased");
+		    		}
+	    		}
+	    		
+	    		totalLogons += logons[i];
+	    		
+	    		i += 1;
+	    	}
+	    	
+	    	System.out.println("Total logons today = " + totalLogons + " on " + uuid);
+			
+			m += 1;
+		}
+		
+		System.out.println("Done");
 	}
 	
 }
