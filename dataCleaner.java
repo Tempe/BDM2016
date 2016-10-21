1ec315984e95d0dc73579fc6050238d379987214
JakobRF <jste@itu.dk>
2016-10-21 10:36:04 +0200

    Add files via upload
    Cleaneren er blevet bedre, den frasorterer nu på flere områder (hvis tidsforskellen er under 60 sek)

--- dataCleaner.java
+++ dataCleaner.java
@@ -1,17 +1,21 @@
-import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import java.io.*;
 import java.util.Iterator;
 
 
+// for output look at https://www.mkyong.com/java/json-simple-example-read-and-write-json/
+
 public class dataCleaner {
 
 	public static void main(String[] args) {
 		
+		boolean skipTime = false;
 		JSONParser parser = new JSONParser();
 		JSONArray device = new JSONArray();
 		
@@ -32,15 +36,21 @@ public class dataCleaner {
 			
 		}
 		
+		int totalTotalLogon = 0;
 		Iterator<String> iterator1 = device.iterator();
 		
 		int m = 0;
 		while (iterator1.hasNext() && m < device.size()) {
+			
+			JSONArray outputArray = new JSONArray();
+			JSONObject outputUuid = new JSONObject();
 		
 			JSONObject readingsObj = (JSONObject) device.get(m); 
 			
 			String uuid = (String) readingsObj.get("uuid");
 			JSONArray readings = (JSONArray) readingsObj.get("Readings");
+			
+			outputUuid.put("uuid", uuid);
 			
 			// JSONObject uuid = (JSONObject) device.get(1);
 			
@@ -52,7 +62,7 @@ public class dataCleaner {
 	    	long[] timestamp = new long[readings.size()+1];
 	    	long[] logons = new long[readings.size()+1];
 	    	
-	    	int totalLogons = 0;
+	    	long totalLogons = 0;
 	    	
 	    	Iterator<String> iterator2 = readings.iterator();
 	    	
@@ -66,23 +76,38 @@ public class dataCleaner {
 	    		//System.out.println("Values " + values);
 	    		//System.out.println("Timestamp " + timestamp[i] + " logons " + logons[i]);
 	    		
-	    		if (i > 0) {							// 30 sec
-		    		if (timestamp[i] - timestamp[i-1] < 30000) {
-		    			System.out.println("Input " + i + " should be erased");
+	    		if (i > 0) {				
+		    		if (timestamp[i] - timestamp[i-1] == 0) {
+		    			// they are equal, skip timestamp
+		    			skipTime = true;
+		    		}
+		    											// 1 minute
+		    		if (timestamp[i] - timestamp[i-1] < 60000) {
+		    			// there is less than 1 minute between the two timestamps
+		    			skipTime = true;
 		    		}
 	    		}
 	    		
-	    		totalLogons += logons[i];
+	    		if (!skipTime) {
+	    			totalLogons += logons[i];
+	    			
+	    			outputArray.add(values);	// add object to output array
+	    		}
 	    		
+	    		skipTime = false; // reset
 	    		i += 1;
 	    	}
 	    	
 	    	System.out.println("Total logons today = " + totalLogons + " on " + uuid);
 			
+	    	totalTotalLogon += totalLogons;
+	    	
 			m += 1;
+			
+			System.out.println("Output " + outputArray);
+			//writeToFile
 		}
 		
-		System.out.println("Done");
+		System.out.println("Done with " + totalTotalLogon);
 	}
-	
 }
