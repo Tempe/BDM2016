1d919a9517f9df487a5869f5945d52f81c248ae1
Jan Vium Enghoff <jan.jve@gmail.com>
2016-11-01 15:34:15 +0100

    mapper types

--- DataCleaning/Workspace/mapreduce/src/main/java/dk/itu/mapreduce/Driver.java
+++ DataCleaning/Workspace/mapreduce/src/main/java/dk/itu/mapreduce/Driver.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.OutputFormat;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -12,7 +16,25 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Driver {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-
+		int mapperType = Integer.parseInt(args[0]);
+		String inputPath = args[0];
+		String outputPath = args[0];
+		switch(mapperType){
+			case 0:
+				startJob(FirstMapper.class, FirstReducer.class, TextInputFormat.class, TextOutputFormat.class, inputPath, outputPath);
+				break;
+			default:
+				throw new IOException("Unkown mapper type");
+		}
+	}
+	
+	public static void startJob(Class<? extends Mapper> mapper,
+			Class<? extends Reducer> reducer,
+			Class<? extends InputFormat> inputFormat, 
+			Class<? extends OutputFormat> outputFormat,
+			String inputPath,
+			String outputPath) throws IOException, ClassNotFoundException, InterruptedException {
+		
 		Configuration conf = new Configuration();
 		Job job = new Job(conf, "First Mapreduce App");
 
@@ -20,19 +42,19 @@ public class Driver {
 		job.setJarByClass(Driver.class);
 
 		// Set Input & Output Format
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setInputFormatClass(inputFormat);
+		job.setOutputFormatClass(outputFormat);
 
 		// Set Mapper & Reducer Class
-		job.setMapperClass(FirstMapper.class);
-		job.setReducerClass(FirstReducer.class);
+		job.setMapperClass(mapper);
+		job.setReducerClass(reducer);
 
 		// No. of reduce tasks, equals no. output file
 		job.setNumReduceTasks(1);
 
 		// HDFS input and output path
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.addInputPath(job, new Path(inputPath));
+		FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
 		job.waitForCompletion(true);
 	}
